785940f4237100e7bf7bab3f52727abff47609c2
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-07-05 22:25:06 +0300

    added

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+//change header to fixed & change it's color
 window.onscroll = ()=> {makeHeaderSticky()};
 
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
+var sticky_header = document.getElementById("sticky_header");
+var main__wrapper = document.getElementById("main__wrapper");
 
 function makeHeaderSticky() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    sticky_header.classList.add("change_header_color");
+    main__wrapper.classList.add("main__wrapper_padding");
   } else {
     header.classList.remove("sticky");
+    sticky_header.classList.remove("change_header_color");
+    main__wrapper.classList.remove("main__wrapper_padding");
   }
 }
