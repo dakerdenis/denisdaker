27b1100d52430dae277161f0f857b67ecf813f58
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-09-21 15:45:10 +0400

    added

--- index.js
+++ index.js
@@ -18,77 +18,91 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 //************************************************
 
-    function validateForm() {
-        var errorMessages = document.getElementById("errorMessages");
-        var closeButton = document.querySelector(".close-button");
-        errorMessages.innerHTML = ''; // Clear previous error messages
+var contact__form = document.getElementById("contact__form");
 
-        // Check if the name field is filled
-        var nameInput = document.querySelector(".contact__form__input__input input[placeholder='Your name']");
-        var name = nameInput.value.trim();
-        
-        // Check if the number field is filled
-        var numberInput = document.querySelector(".contact__form__input__input input[placeholder='Your mobile number']");
-        var number = numberInput.value.trim();
+contact__form.addEventListener("submit",(e)=>{
+    e.preventDefault();
 
-        // Check if the email field is filled
-        var emailInput = document.querySelector(".contact__form__input__input input[placeholder='Your e-mail address']");
-        var email = emailInput.value.trim();
+    var errorMessages = document.getElementById("errorMessages");
+    var closeButton = document.querySelector(".close-button");
+    errorMessages.innerHTML = ''; // Clear previous error messages
 
-        // Check if the message textarea is filled
-        var messageTextarea = document.querySelector(".contact__form__input__textarea textarea");
-        var message = messageTextarea.value.trim();
 
-        // Check if at least one radio button is checked
-        var radioButtons = document.querySelectorAll(".contact__form__input__radio input[name='radio-group']");
-        var isRadioChecked = false;
+    // Check if the name field is filled
+    var nameInput = document.querySelector(".contact__form__input__input input[placeholder='Your name']");
+    var name = nameInput.value.trim();
+    
+    // Check if the number field is filled
+    var numberInput = document.querySelector(".contact__form__input__input input[placeholder='Your mobile number']");
+    var number = numberInput.value.trim();
 
-        for (var i = 0; i < radioButtons.length; i++) {
-            if (radioButtons[i].checked) {
-                isRadioChecked = true;
-                break;
-            }
+    // Check if the email field is filled
+    var emailInput = document.querySelector(".contact__form__input__input input[placeholder='Your e-mail address']");
+    var email = emailInput.value.trim();
+
+    // Check if the message textarea is filled
+    var messageTextarea = document.querySelector(".contact__form__input__textarea textarea");
+    var message = messageTextarea.value.trim();
+
+    // Check if at least one radio button is checked
+    var radioButtons = document.querySelectorAll(".contact__form__input__radio input[name='radio-group']");
+    
+    var isRadioChecked = false;
+
+    for (var i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            isRadioChecked = true;
+            break;
         }
-
-        // Check if the consent checkbox is checked
-        var consentCheckbox = document.getElementById("consentCheckbox");
-        var isConsentChecked = consentCheckbox.checked;
-
-        // Validate all fields
-        if (name === "" || number === "" || email === "" || message === "" || !isRadioChecked || !isConsentChecked) {
-            // Display error messages in the errorMessages div
-            if (name === "") {
-                errorMessages.innerHTML += "<p>Name is required.</p>";
-            }
-            if (number === "") {
-                errorMessages.innerHTML += "<p>Number is required.</p>";
-            }
-            if (email === "") {
-                errorMessages.innerHTML += "<p>Email is required.</p>";
-            }
-            if (message === "") {
-                errorMessages.innerHTML += "<p>Message is required.</p>";
-            }
-            if (!isRadioChecked) {
-                errorMessages.innerHTML += "<p>Please select a contact method.</p>";
-            }
-            if (!isConsentChecked) {
-                errorMessages.innerHTML += "<p>Please agree to the terms.</p>";
-            }
-
-            errorMessages.style.display = 'block'; // Show the error message box
-            closeButton.style.display = 'block'; // Show the close button
-            errorMessages.innerHTML +='<span class="close-button" onclick="closeErrorMessages()">X</span>';
-            return false;
-        }
-
-        // If all fields are filled and checkbox is checked, allow form submission
-        return true;
     }
 
-    // Function to close the error message box
-    function closeErrorMessages() {
-        var errorMessages = document.getElementById("errorMessages");
-        errorMessages.style.display = 'none'; // Hide the error message box
+    // Check if the consent checkbox is checked
+    var consentCheckbox = document.getElementById("consentCheckbox");
+    var isConsentChecked = consentCheckbox.checked;
+
+    var formContent = false;
+     // Validate all fields
+     if (name === "" || number === "" || email === "" || message === "" || !isRadioChecked || !isConsentChecked) {
+        // Display error messages in the errorMessages div
+        if (name === "") {
+            errorMessages.innerHTML += "<p>Name is required.</p>";
+        }
+        if (number === "") {
+            errorMessages.innerHTML += "<p>Number is required.</p>";
+        }
+        if (email === "") {
+            errorMessages.innerHTML += "<p>Email is required.</p>";
+        }
+        if (message === "") {
+            errorMessages.innerHTML += "<p>Message is required.</p>";
+        }
+        if (!isRadioChecked) {
+            errorMessages.innerHTML += "<p>Please select a contact method.</p>";
+        }
+        if (!isConsentChecked) {
+            errorMessages.innerHTML += "<p>Please agree to the terms.</p>";
+        }
+
+        errorMessages.style.display = 'block'; // Show the error message box
+        closeButton.style.display = 'block'; // Show the close button
+        errorMessages.innerHTML+='<span class="close-button" onclick="closeErrorMessages()">X</span>';
+
+        var contact__form__container = document.getElementById("contact__form__container");
+        // Hide the error message div after 2 seconds
+        setTimeout(function () {
+            errorMessages.style.display = 'none';
+            closeButton.style.display = 'none'; // Hide the close button
+        }, 3000);
+
+        formContent =  false;
+    } else {
+        formContent = true;
     }
 
+
+});
+
+function closeErrorMessages() {
+    var errorMessages = document.getElementById("errorMessages");
+    errorMessages.style.display = 'none'; // Hide the error message box
+};
