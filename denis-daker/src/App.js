d8305e2f9e7872dd1f86b34b0b89563555738478
Denis Akershteyn <dakerdenis@gmail.com>
2024-07-17 21:56:13 +0300

    added

--- denis-daker/src/App.js
+++ denis-daker/src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
-import Header from './components/Header'; 
-import Hero from './components/Hero';
-
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
 function App() {
   return (
     <div className="App">
       <div className="main__wrapper">
         <div className="main__container">
           {/**header***/}
-         <Header /> 
+          <Header />
 
-        {/**SCREEN WITH SOCIALS NAME and DESCription***/}
+          {/**SCREEN WITH SOCIALS NAME and DESCription***/}
           <Hero />
 
+          {/**ABOUT ME***/}
+          <About />
 
+          {/**SERVICES**/}
+          <Services />
 
-
-
+          
         </div>
       </div>
     </div>
---
+++ denis-daker/src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/hero.css";
+
+const Hero = () => {
+  return (
+    <section id="about">
+      <div className="section__about__container" />
+    </section>
+  );
+};
+
+export default Hero;
