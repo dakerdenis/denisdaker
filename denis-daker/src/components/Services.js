5c74e0b4defe6c6ff317941725129420a7c1a180
Denis Akershteyn <dakerdenis@gmail.com>
2024-07-21 09:53:15 +0300

    added

--- denis-daker/src/components/Services.js
+++ denis-daker/src/components/Services.js
@@ -3,29 +3,29 @@ import "../styles/hero.css";
 import "../styles/services.css";
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.min.css';
-import 'swiper/swiper.min.css';
-import SwiperCore, { FreeMode, Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/free-mode';
+import { Navigation, Pagination, FreeMode } from 'swiper';
 
-
-// Install Swiper modules
-SwiperCore.use([FreeMode, Navigation, Pagination]);
+// No need to call SwiperCore.use([])
 
 const Services = () => {
   return (
-    <section id="about">
-      <div className="section__services__container" >
-            <div className="services__name">
-            Services <span>.</span>
-            </div>
-
-            <div className="services__wrapper__swiper">
-            <Swiper
+    <section id="services">
+      <div className="section__services__container">
+        <div className="services__name">
+          Services <span>.</span>
+        </div>
+        <div className="services__wrapper__swiper">
+          <Swiper
             spaceBetween={50}
             slidesPerView={3}
             freeMode={true}
             navigation={true}
             pagination={{ clickable: true }}
+            modules={[FreeMode, Navigation, Pagination]} // Registering modules here
           >
             <SwiperSlide>Slide 1</SwiperSlide>
             <SwiperSlide>Slide 2</SwiperSlide>
@@ -33,7 +33,7 @@ const Services = () => {
             <SwiperSlide>Slide 4</SwiperSlide>
             {/* Add more slides as needed */}
           </Swiper>
-            </div>
+        </div>
       </div>
     </section>
   );
