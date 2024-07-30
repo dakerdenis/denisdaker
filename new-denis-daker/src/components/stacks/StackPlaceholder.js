c70fec9409709e1dc9c8a167b6c63abf78b8e032
Denis Akershteyn <dakerdenis@gmail.com>
2024-07-30 22:41:28 +0300

    added

--- new-denis-daker/src/components/stacks/StackPlaceholder.js
+++ new-denis-daker/src/components/stacks/StackPlaceholder.js
@@ -3,7 +3,7 @@ import React from "react";
 const StackPlaceholder = () => {
   return (
     <div className="stack__placeholder__container">
-      PIZDA
+      I went from a beginner student to an experienced developer, mastering a number of key technologies and frameworks. In 2 years, my knowledge has expanded to include HTML, CSS, JavaScript, Python, TypeScript and ReactJS. These languages ​​and tools allowed me to create lively and dynamic web projects, developing skills from layout and styling to creating interactive features and full-fledged applications
     </div>
   );
 };
