e84b8d38b3c033f20a5b2139bb38e41c7cb9fa0f
Denis Akershteyn <dakerdenis@gmail.com>
2024-07-30 22:06:53 +0300

    added

--- new-denis-daker/src/components/Stack.js
+++ new-denis-daker/src/components/Stack.js
@@ -2,13 +2,30 @@ import React from "react";
 
 import "../styles/stack.css";
 
+import dot from "../assets/dot.svg";
 
+import StackElement from "./stacks/StackElement";
 const Stack = () => {
   return (
     <section id="stack">
-        <div className="stack__container">
-
+      <div className="stack__container">
+        <div className="stack__name">
+          <p>Stack</p>
+          <span>
+            <img src={dot} alt="" />
+          </span>
         </div>
+
+
+          <div className="stack__elements__wrapper">
+                      <StackElement/>
+                      <StackElement/>
+                      <StackElement/>
+                      <StackElement/>
+                      <StackElement/>
+
+          </div>
+      </div>
     </section>
   );
 };
---
+++ new-denis-daker/src/components/stacks/StackElement.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+
+
+const StackElement = () => {
+  return (
+    <div className="stack__element__container">
+      <div className="stack__element__percentage">70%</div>
+      <div className="stack__element__name">HTML/CSS</div>
+
+      <div className="stack__element__raiting">
+        <div className="stack__rating__block"></div>
+        <div className="stack__rating__block"></div>
+        <div className="stack__rating__block"></div>
+        <div className="stack__rating__block"></div>
+        <div className="stack__rating__block"></div>
+        <div className="stack__rating__block"></div>
+        <div className="stack__rating__block"></div>
+        <div className="stack__rating__block"></div>
+        <div className="stack__rating__block"></div>
+        <div className="stack__rating__block"></div>
+      </div>
+    </div>
+  );
+};
+
+export default StackElement;
