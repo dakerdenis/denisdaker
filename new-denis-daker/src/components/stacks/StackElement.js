e646fb1e6797edad135ccbc512753a0df4297a25
Denis Akershteyn <dakerdenis@gmail.com>
2024-08-05 19:44:07 +0300

    added

--- new-denis-daker/src/components/stacks/StackElement.js
+++ new-denis-daker/src/components/stacks/StackElement.js
@@ -3,12 +3,10 @@ import React from "react";
 const StackElement = ({ name, percentage, rating }) => {
   return (
     <div className="stack__element__container">
-      <div className="stack__element__background">
-
-      </div>
+      <div className="stack__element__background"></div>
 
       <div className="stack__element__data">
-      <div className="stack__element__percentage">{percentage}%</div>
+        <div className="stack__element__percentage">{percentage}%</div>
         <div className="stack__element__name">{name}</div>
 
         <div className="stack__element__raiting">
