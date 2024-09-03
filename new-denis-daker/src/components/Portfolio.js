c2f6b12ab63b5c000cceb787fc495b92af4a3fab
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2024-09-03 09:46:21 +0300

    added

--- new-denis-daker/src/components/Portfolio.js
+++ new-denis-daker/src/components/Portfolio.js
@@ -8,6 +8,14 @@ import "swiper/css";
 import "swiper/css/free-mode";
 import { FreeMode } from "swiper/modules";
 
+
+
+
+/***PORTFOLIO IMAGES****/
+import portfolio1 from "../assets/portfolio/1.png"; 
+
+import quotes from '../assets/portfolio/quotes.svg';
+
 import link from "../assets/swiper/link.svg";
 const Portfolio = () => {
     const [spaceBetween, setSpaceBetween] = useState(43); // Fixed space between slides
@@ -58,21 +66,39 @@ const Portfolio = () => {
                                 <div className="swiper__block_porfolio">
                                     {/* Slide content goes here */}
                                     <div className="swiper__image__container swiper__image__container1">
-
+                                        <img src={portfolio1} alt="" />
                                     </div>
 
 
+                                    
+                                    
+                                    <div className="swiper__portfolio__quotes">
+                                        <img src={quotes} alt="" />
+                                    </div>
+
+                                    
+                                    <div className="swiper__portfolio__desc">
+                                        <p>
+                                        The site was developed for an Architectural advertising agency with more than 20 years of experience in the Azerbaijani market and abroad. 
+                                        The company didn't have a website, and our team was tasked with incorporating the company's years of experience and history into a simple, easy-to-use portfolio
+                                        </p>
+                                        
+                                        <p>
+                                        The design and concept were developed on a turnkey basis in accordance with all the wishes and requirements of the client. 
+                                        The site is designed in two color modes - day and night and in two languages, and an admin panel was developed for complete content management from the client.
+                                        </p>
+                                    </div>
                                     <div className="swiper__border__left"></div>
                                     <div className="swiper__border__right swiper__border__right1"></div>
                                     
 
 
                                     <div className="swiper__button">
-                                        <a target="_blank" href="#">
+                                        <a target="_blank" href="https://altaysel.az/">
                                             <div className="swiper__button_image swiper__button_image1">
                                                 <img src={link} alt="" />
                                             </div>
-                                            <p>ati.az</p>
+                                            <p>altaysel.az</p>
                                         </a>
                                     </div>
                                 </div>
