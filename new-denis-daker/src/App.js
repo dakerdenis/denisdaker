513e0528c1231de8a803c28095a7b767c1acbce5
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2024-09-10 14:44:41 +0300

    added

--- new-denis-daker/src/App.js
+++ new-denis-daker/src/App.js
@@ -22,33 +22,10 @@ function App() {
           {/**ABOUT ME***/}
           <About />
 
-          {/**SERVICES**/}
-          <Services />
-
-          {/**STACK**/}
-          <Stack />
-
-          <div className="main__background__placeholder">
-          <div className="main__placeholder__background">
-
-            </div>
-            <div className="main__placeholder__blur"></div>
-            <div className="main__placeholder__content">
-              {/**SOFT**/}
-              <Soft />
-
-              {/**PORTFOLIO**/}
-              <Portfolio />
-
-              {/***CONTACT***/}
-              <Contact />
-            </div>
 
 
 
 
-
-          </div>
 
         </div>
       </div>
---
+++ new-denis-daker/src/components/app_backlog.js
@@ -0,0 +1,21 @@
+          {/**SERVICES**/}
+          <Services />
+
+          {/**STACK**/}
+          <Stack />
+
+          <div className="main__background__placeholder">
+          <div className="main__placeholder__background">
+            </div>
+            <div className="main__placeholder__blur"></div>
+            <div className="main__placeholder__content">
+              {/**SOFT**/}
+              <Soft />
+
+              {/**PORTFOLIO**/}
+              <Portfolio />
+
+              {/***CONTACT***/}
+              <Contact />
+            </div>
+          </div>
