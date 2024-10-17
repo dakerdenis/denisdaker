d02c7b8f4b5576d91cae52c2973da56cd581badc
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2024-10-17 10:49:44 +0400

    added

--- new-denis-daker/src/hooks/useDynamicStyles.js
+++ new-denis-daker/src/hooks/useDynamicStyles.js
@@ -15,16 +15,26 @@ export const useDynamicStyles = () => {
       link.rel = 'stylesheet';
       link.href = stylesheetPath;
       link.id = 'language-style';
+
+      // Make sure it's appended after all other stylesheets
       document.head.appendChild(link);
     };
 
     // Apply the correct CSS based on the current language
     if (i18n.language === 'ru') {
       applyStylesheet(`${process.env.PUBLIC_URL}/styles/style_ru.css`);
-    } else if (i18n.language === 'en') {
-      applyStylesheet(`${process.env.PUBLIC_URL}/styles/style_en.css`);
+    } else if (i18n.language === 'az') {
+      applyStylesheet(`${process.env.PUBLIC_URL}/styles/style_az.css`);
     } else {
       applyStylesheet(`${process.env.PUBLIC_URL}/styles/style_en.css`); // Default to English
     }
+
+    // Clean up: remove the stylesheet when language changes
+    return () => {
+      const stylesheet = document.getElementById('language-style');
+      if (stylesheet) {
+        stylesheet.remove();
+      }
+    };
   }, [i18n.language]);
 };
